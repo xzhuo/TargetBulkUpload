86c682a297e9f47ed63a09ce6f9fa7e057a316d7
xzhuo <yunkzo@gmail.com>
2017-10-02 10:37:08 -0500

    how to call records in sheet_data.

--- submission_oo.py
+++ submission_oo.py
@@ -574,6 +574,10 @@ class SheetData:
         self.name = sheet_name
         self.meta_structure = meta_structure
         self.all_records = []
+        # maybe use iterator for records? see:
+        # http://anandology.com/python-practice-book/iterators.html
+        # eg.
+        # http://biopython.org/DIST/docs/api/Bio.Align-pysrc.html#MultipleSeqAlignment
 
     def add_record(self, row_data):
         self.all_records.append(row_data)
