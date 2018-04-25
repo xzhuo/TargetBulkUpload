0b1f4b02e67626522ffe777859044e2f30044daa
xzhuo <yunkzo@gmail.com>
2018-04-25 15:01:26 -0500

    ER diagram

---
+++ erd.py
@@ -0,0 +1,55 @@
+import metastructure
+import argparse
+import logging
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--is_production',
+        '-p',
+        action="store_true",
+        dest="is_production",
+        default="is_production",
+        help="test flag. default option is true, which will get records from the test database. \
+        The metadata only fetch records from the production database if this option is TRUE.\n",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    logging.getLogger().setLevel(logging.INFO)
+    is_production = args.is_production
+    try:
+        meta_structure = metastructure.MetaStructure(is_production)
+    except metastructure.StructureError as structure_error:
+        logging.error(structure_error)
+
+    print("title {label: \"TaRGET metadata diagram\", size: \"20\"}\n\n# Entities\n")
+    for sheet_name in meta_structure.schema_dict.keys():
+        # print category
+        print("[%s]" % sheet_name)
+        sheet_schema = meta_structure.get_sheet_schema(sheet_name)
+        for m in range(0, len(sheet_schema)):
+            name = sheet_schema[m]['name']
+            display_name = sheet_schema[m]['text']
+            if not (name == "accession" or name == "user_accession"):
+                print("  %s {label: \"%s\"}" % (name, display_name))
+    print("\n# Relationships\n")
+    for sheet_name in meta_structure.schema_dict.keys():
+        sheet_relationships = meta_structure.get_sheet_link(sheet_name)
+        for n in range(0, len(sheet_relationships['connections'])):
+            link_dict = sheet_relationships['connections'][n]
+            if 'allow_multiple' in link_dict and link_dict['allow_multiple']:
+                to_symbol = "*"
+            else:
+                to_symbol = "?"
+            # We don't have required info for relationships yet, so the symbol will be either * or +
+            connect_to = meta_structure.category_to_sheet_name[link_dict['to']]
+            print("%-12s*--%s %s {label: \"%s\"}" % (sheet_name, to_symbol, connect_to, link_dict['name']))
+
+
+if __name__ == "__main__":
+    main()
